dcc5c646c500508d894dcb0adbd86cab2575f400
khai-1711 <khai129031@gmail.com>
2023-10-14 10:24:30 +0700

    update sth

--- src/routes/controllers/userController.ts
+++ src/routes/controllers/userController.ts
@@ -3,6 +3,29 @@ import Users, { IUser } from '../../models/users';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
+let refreshtokens: Array<string | number> = [];
+// Generate Access Token
+const generateAccessToken = (user) => {
+    return jwt.sign(
+        {
+            id: user.id,
+            username: user.username,
+        },
+        process.env.MY_SEC_KEY,
+        { expiresIn: '30s' },
+    );
+};
+
+const refreshAccessToken = (user) => {
+    return jwt.sign(
+        {
+            id: user.id,
+            username: user.username,
+        },
+        process.env.MY_REFRESH_KEY,
+        { expiresIn: '30d' },
+    );
+};
 // Register routes
 export const registerUser = async (req: Request, res: Response) => {
     try {
@@ -31,9 +54,9 @@ export const registerUser = async (req: Request, res: Response) => {
 // get all user
 export const getAllUser = async (req: Request, res: Response) => {
     try {
-        const users = await Users.find();
+        const user = await Users.find().select('-passwd');
 
-        return res.status(200).json(users);
+        return res.status(200).json(user);
     } catch (error) {
         console.log(error);
         return res.sendStatus(500);
@@ -48,7 +71,9 @@ export const findUserByName = async (req: Request, res: Response) => {
         if (!user) {
             return res.status(404).json({ message: 'Người dùng không tồn tại' });
         }
-        return res.status(200).json(user);
+        const a = { ...user.toObject() };
+        delete a.passwd;
+        return res.status(200).json(a);
     } catch (error) {
         console.log(error);
         return res.sendStatus(500);
@@ -64,14 +89,14 @@ export const login = async (req: Request, res: Response) => {
             const isPasswordValid: string = await bcrypt.compare(passwd, user.passwd);
 
             if (isPasswordValid) {
-                const ac = jwt.sign(
-                    {
-                        id: user.id,
-                        username: user.username,
-                    },
-                    process.env.MY_SEC_KEY,
-                    { expiresIn: '30s' },
-                );
+                const ac = generateAccessToken(user);
+                const refreshToken = refreshAccessToken(user);
+                res.cookie('refreshToken', refreshToken, {
+                    httpOnly: true,
+                    secure: false,
+                    path: '/',
+                    sameSite: 'strict',
+                });
                 const a = { ...user.toObject() };
                 delete a.passwd;
                 return res.status(200).json({ user: a, ac });
@@ -82,4 +107,30 @@ export const login = async (req: Request, res: Response) => {
         console.log(error);
         return res.sendStatus(500);
     }
+};
+
+// Refresh Token
+export const requestRefreshToken = async (req: Request, res: Response) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) return res.status(401).json('You are not Authenticated');
+    if (!refreshtokens.includes(refreshToken)) {
+        return res.status(403).json('Refresh Token is not valid');
+    }
+    jwt.verify(refreshToken, process.env.MY_REFRESH_KEY, (err, user) => {
+        if (err) {
+            console.log(err);
+        }
+        refreshtokens = refreshtokens.filter((token) => token !== refreshToken);
+        //create new access Token
+        const newAccessToken = generateAccessToken(user);
+        const newRefreshToken = refreshAccessToken(user);
+        refreshtokens.push(newRefreshToken);
+        res.cookie('refreshToken', newRefreshToken, {
+            httpOnly: true,
+            secure: false,
+            path: '/',
+            sameSite: 'strict',
+        });
+        res.status(200).json({ AccessToken: newAccessToken });
+    });
 };
